b77b12ccffbc572a6d620d296b6e4f4f789b76b6
daiya <daiya06010@gmail.com>
2020-08-03 16:56:19 +0900

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get '/about',   to: 'static_pages#about'
-  get '/signup',  to: 'users#new'
+  root   'static_pages#home'
+  get    '/about',   to: 'static_pages#about'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+ 
   # root -> ルートパス(localhost:3000)でアクセスした場合に割り当てられる設定で、Controllerファイルに記述するindexメソッドを実行するように定義
   get '/posts', to: 'posts#index'
 
